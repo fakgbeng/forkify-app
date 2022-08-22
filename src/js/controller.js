8b3fe492a2f08aef1500a333c11d4b93185a1292
fakgbeng <temris123@gmail.com>
2022-08-22 14:39:32 +0100

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -113,6 +113,10 @@ const controlAddRecipe = async function (newRecipe) {
     addRecipeView.renderError(err.message);
   }
 };
+const newFeature = function () {
+  console.log('Welcome to the New Code');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -121,8 +125,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
-  alert('HACKED');
+  newFeature();
 };
 init();
 // window.addEventListener('hashchange', controlRecipes)
