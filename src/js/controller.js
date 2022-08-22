9cd07e9a81c71aff3c2e0f8f447ab6a277f34e35
fakgbeng <temris123@gmail.com>
2022-08-22 14:27:54 +0100

    New feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -122,6 +122,7 @@ const init = function () {
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
   console.log('Welcome');
+  alert('HACKED');
 };
 init();
 // window.addEventListener('hashchange', controlRecipes)
